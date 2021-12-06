efdbbfeef1cc68783d3e143b7b0fae94dfb855bc
dschaub95 <schaub.darius@gmail.com>
2021-12-06 16:19:38 +0100

    added donwload script

---
+++ download_data.py
@@ -0,0 +1,3 @@
+from googleDriveFileDownloader import googleDriveFileDownloader
+gdownloader = googleDriveFileDownloader()
+gdownloader.downloadFile("https://drive.google.com/uc?id=152mpCze-v4d0m9kdsCeVkLdHFkjeDeF5&export=download")
